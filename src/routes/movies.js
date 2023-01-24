a6461a9cd9a8144cb15eee03e2dbe35b31e72180
Martin <martinikoh10@gmail.com>
2023-01-24 01:09:44 +0100

    delete method and unique ids added

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -1,7 +1,10 @@
 const { Router } = require('express');
 const router = Router();
 
-const data = require('../movies.json')
+const _ = require('underscore');
+const { v4: uuidv4 } = require('uuid');
+
+const data = require('../movies.json');
 
 router.get('/', (req, res) => {
     res.json(data);
@@ -10,14 +13,23 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
     const { title, director, year, rating } = req.body;
     if (title && director && year && rating) {
-        const id = data.length + 1;
+        const id = uuidv4();
         const newMovie = {...req.body, id };
         data.push(newMovie);
         res.json(data);
     }
     else {
-        res.status(500).json({ "error": "You must fill all fields" })
+        res.status(500).json({ "error": "You must fill all fields" });
     }
+});
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    const projectIndex = data.findIndex(movie => movie.id == id);
+    
+    data.splice(projectIndex, 1);
+
+    res.json(data)
 });
 
 module.exports = router;
