2e1f4e7697a130b808fa72b0123fda64340efd2f
Martin <martinikoh10@gmail.com>
2023-01-25 01:10:47 +0100

    all basic methods (get, post, put, delete) of movies API REST finished and connected with mongodb

--- src/index.js
+++ src/index.js
@@ -2,20 +2,33 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 
-// Configurations 
-app.set('port', process.env.PORT || 3000);
-app.set('json spaces', 2);
+// This way our whole app is connected
+// with mongo at start and
+// I can use it just using
+// const mongo = require( '../db/conn' );
+// const db = mongo.getDb();
+// db.collection( 'collection' ).find()
 
-// Middleware
-app.use(morgan('dev'));
-app.use(express.urlencoded({extended: false}));
-app.use(express.json());
+const mongo = require('./db/conn')
+mongo.connectToServer((err) => {
+    if(err) throw err
 
-// Routing
-app.use(require('./routes/index'));
-app.use('/api/movies', require('./routes/movies'));
+    // Configurations 
+    app.set('port', process.env.PORT || 3000);
+    app.set('json spaces', 2);
 
-// Server initiation
-app.listen(app.get('port'), ()=>{
-    console.log(`Server listening on port ${app.get('port')}`);
+    // Middleware
+    app.use(morgan('dev'));
+    app.use(express.urlencoded({extended: false}));
+    app.use(express.json());
+
+    // Routing
+    app.use(require('./routes/index'));
+    app.use('/api/movies', require('./routes/movies'));
+
+    // Server initiation
+    app.listen(app.get('port'), ()=>{
+        console.log(`Server listening on port ${app.get('port')}`);
+    });
 });
+
--- src/routes/movies.js
+++ src/routes/movies.js
@@ -6,36 +6,66 @@ const { v4: uuidv4 } = require('uuid');
 
 const data = require('../movies.json');
 const mongo = require('../db/conn')
-// router.get('/', (req, res) => {
-//     res.json(data);
-// });
+const { ObjectId } = require('mongodb')
 
 router.get('/', async function (req, res) {
-    mongo.connectToServer((err) => {
-        if (err){
-            throw err
-        }
-        
-        const db = mongo.getDb();
+    const db = mongo.getDb();
 
-        db.collection('movies').find().toArray((err, result) => {
-            if (err) {
-                res.status(400).send("Error fetching listings!");
-            }
-            else {
-                res.json(result);
-            }
-        });
+    db.collection('movies').find().toArray((err, result) => {
+        if (err) {
+            res.status(400).send("Error fetching listings!");
+        }
+        else {
+            res.json(result);
+        }
     });
 });
 
 router.post('/', (req, res) => {
     const { title, director, year, rating } = req.body;
     if (title && director && year && rating) {
-        const id = uuidv4();
-        const newMovie = {...req.body, id };
-        data.push(newMovie);
-        res.json(data);
+        const db = mongo.getDb();
+        const newMovie = { ...req.body };
+        db.collection('movies').insertOne(newMovie, (err, result) => {
+            if (err) {
+                res.status(400).send("Error fetching listings!");
+            }
+            else {
+                console.log(`Added a new movie with id ${result.insertedId}`);
+                res.status(204).send();
+            }
+        });
+    }
+    else {
+        res.status(500).json({ "error": "You must fill all fields" });
+    }
+});
+
+router.put('/:id', (req, res) => {
+    const { title, director, year, rating } = req.body;
+    if (title && director && year && rating) {
+        const db = mongo.getDb();
+        const objectId = new ObjectId(req.params)
+        const movie ={ _id: objectId };
+        console.log(movie)
+        const update = {
+            $set: {
+                "title": title,
+                "director": director,
+                "year": year,
+                "rating": rating,
+            }
+        };
+
+        db.collection('movies').updateOne(movie, update, (err, _result) => {
+            if (err) {
+                res.status(400).send(`Error updating movie with id: ${movie._id}`);
+            }
+            else {
+                console.log(`movie with id: ${movie._id} updated`);
+                res.status(204).send();
+            }
+        });
     }
     else {
         res.status(500).json({ "error": "You must fill all fields" });
@@ -43,12 +73,19 @@ router.post('/', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-    const { id } = req.params;
-    const projectIndex = data.findIndex(movie => movie.id == id);
-    
-    data.splice(projectIndex, 1);
+    const db = mongo.getDb();
+    const objectId = new ObjectId(req.params);
+    const movie = { _id: objectId };
 
-    res.json(data)
+    db.collection('movies').deleteOne(movie, (err, _result) => {
+        if (err) {
+            res.status(400).send(`Error deleting movie with id: ${movie._id}`);
+        }
+        else {
+            console.log(`movie with id: ${movie._id} deleted`)
+            res.status(204).send()
+        }
+    });
 });
 
 module.exports = router;
